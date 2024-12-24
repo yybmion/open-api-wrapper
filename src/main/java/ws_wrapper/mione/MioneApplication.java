11b6560e2ccfb1a458c1bd0cf12744a75e063406
yoobin_mion <yyb400@naver.com>
2024-12-24 10:41:21 +0900

    git connect

---
+++ src/main/java/ws_wrapper/mione/MioneApplication.java
@@ -0,0 +1,13 @@
+package ws_wrapper.mione;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MioneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MioneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws_wrapper/mione/MioneApplicationTests.java
@@ -0,0 +1,13 @@
+package ws_wrapper.mione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
